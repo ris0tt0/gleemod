eff6d22aed98375de39232f6b777b1cd0ceaca1b
j@jonathangee.com <j@jonathangee.com>
2018-11-12 14:28:21 -0800

    added index.js to export classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const Cell = require('./src/cell');
+const Controller = require('./src/controller');
+const Model = require('./src/model');
+
+exports.gleemod = {
+	Cell:Cell,
+	Controller:Controller,
+	Model:Model
+}
