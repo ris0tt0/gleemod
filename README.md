3a6a1fd25002fe5c219ccfd967eb66e9ab125091
jay gee <j@jonathangee.com>
2018-11-22 22:56:07 -0800

    updated requirements, added webpack config for debug and production

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gleemod
 basic match 3 controller and model
+
+Requires nodejs 10.13.0
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const Logger = require('js-logger');
 // const Controller = require('./controller');
 import Controller from './controller';
 import Cell from './cell';
-import model from './model';
+import Model from './model';
 
 
+export {Controller, Cell, Model}
 // module.exports = {Controller};
